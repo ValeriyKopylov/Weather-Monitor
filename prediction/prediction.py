cb0cfddb83fff7dc8fea1e4770e31805566a0141
tsyshnatiy <vladimir.tsishnatiy@akvelon.com>
2015-02-27 07:18:43 +0300

    Modifies prediction. Explicitly defines predict function, fixes filtering

--- prediction/prediction.py
+++ prediction/prediction.py
@@ -3,22 +3,70 @@ from spectrum import *
 import numpy as np
 from scipy import signal
 
+
+def predict(x, P, Fs, Fc):
+    #discretization period
+    T = np.float128(1 / Fs)
+
+    # total length of output signal
+    L = len(x) + P
+
+    # time axis of input + predicted samples
+    tp = T * np.linspace(0, L - 1, L)
+
+    # order of regression model
+    N = P / 10
+
+    # limit regression order
+    if (N < 50):
+        N = 50
+    if (N > 400):
+        N = 400
+
+    # design filter using z-transform. we need firwin lowpass filter
+    A = 1
+    B = signal.firwin(50, cutoff=Fc, window='hamming')
+
+    # apply filter
+    x = signal.lfilter(B, A, x)
+
+    # compute regression coefficients.
+    # TODO handle rho less than 0 exception. Try to predict by smaller interval in the future
+    [A, E, K] = arburg(x, N)
+
+    # allocate memory for output
+    y = np.zeros(L)
+
+    # fill part of the output with known part
+    y[0:len(x)] = x
+
+    # apply regression model to the signal.
+    # actually this is IIR filter.
+    # use lfilter func in future.
+    for i in range(len(x), L):
+        y[i] = -1 * np.sum(np.real(A) * y[i-1:i-1-N:-1])
+        
+    return tp, y
+
 plt.close('all')
 
-# point where to start prediction
-M = 30000
-# number of samples in extrapolated time series1
-P = 50000
-# order of regression model
-N = (P - M) / 10
 # all plots will be there
 fig1 = plt.figure()
+
+# point where to start prediction
+M = 3000
+# number of samples in extrapolated time series1
+P = 5000
+
 # discretization frequency
 Fs = np.float128(1 / 15.0)
+
 # discretization period
 T = np.float128(1 / Fs)
+
 # time axis of input + predicted samples
-tp = T * np.linspace(0, P - 1, P)
+ti = T * np.linspace(0, M - 1, M)
+
 # input signal
 secs_in_day = 86400
 secs_in_month = 86400 * 30
@@ -32,34 +80,26 @@ secs_in_year = secs_in_month * 12
 FC = Fs / secs_in_day #Hz
 
 # signal itself
+x = np.sin(2*np.pi*ti/secs_in_day)\
+    + np.sin(2*np.pi*ti/secs_in_month)\
+    + np.sin(2*np.pi*ti/secs_in_year)
+
+# generate additive noise
+noise = 0.03 * np.random.randn(1, M)
+x = x + noise[0]
+# plot noised guy
+plt.plot(ti, x)
+
+# create figure for results
+fig2 = plt.figure()
+# do the job
+tp, y = predict(x, P - M, Fs, FC)
+
+# compare with continued signal
+# TODO remove this during integration with db
 x = np.sin(2*np.pi*tp/secs_in_day)\
     + np.sin(2*np.pi*tp/secs_in_month)\
     + np.sin(2*np.pi*tp/secs_in_year)
-
-# generate additive noise
-noise = 0.03 * np.random.randn(1, P)
-x = x + noise[0]
-# plot noised guy
-plt.plot(tp[0:P], x)
-fig2 = plt.figure()
-# design filter using z-transform. we need firwin lowpass filter
-filter_denominator = 1
-filter_numerator = signal.firwin(N, cutoff=FC, window='hamming')
-# apply filter
-x = signal.lfilter(filter_numerator, filter_denominator,  x)
-# compute regression coefficients.
-# TODO handle rho less than 0 exception. Try to predict by smaller interval in the future
-[A, E, K] = arburg(x, N)
-# allocate memory for output
-y = np.zeros(P)
-# fill part of the output with known part
-y[0:M] = x[0:M]
-
-# apply regression model to the signal.
-# actually this is IIR filter.
-# use lfilter func in future.
-for i in range(M, P):
-    y[i] = -1 * np.sum(A * y[i-1:i-1-N:-1])
 
 # plot results
 plt.plot(tp, x, tp, y)
