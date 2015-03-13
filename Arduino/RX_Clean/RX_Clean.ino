ebf754efd9582520aeaeeda0e1d313314aab37c2
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2015-03-13 15:52:49 +0300

    Working Outpost-Carrier pair

---
+++ Arduino/RX_Clean/RX_Clean.ino
@@ -0,0 +1,45 @@
+#include <SPI.h>
+#include <RF24.h>
+
+#define RF_CS 9
+#define RF_CSN 10
+RF24 radio(RF_CS, RF_CSN);
+const uint64_t pipes[2] = { 0xe7e7e7e7e7LL, 0xc2c2c2c2c2LL };
+
+static const uint16_t OutpostId = 0x01;
+static const uint16_t SensorNumber = 0x06;
+static const uint16_t PacketSize = 0x0D;
+static const byte TempSensorId = 0x01;
+static const byte GasSensorId = 0x02;
+static const byte HumSensorId = 0x03;
+static const byte LightSensorId = 0x04;
+static const byte PressSensorId = 0x05;
+
+float temperature;
+float gas;
+float humidity;
+float light;
+float pressure;
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.printDetails();
+  radio.openWritingPipe(pipes[1]); // note that our pipes are the same above, but that
+  radio.openReadingPipe(1, pipes[0]); // they are flipped between rx and tx sides.
+  radio.startListening();
+}
+
+void loop() {
+  if (radio.available()) {
+    Serial.println("inRadio");
+    byte rx_data[sizeof(uint16_t) + 3 * sizeof(float)];
+    bool done = false;
+    while (!done) {
+      done = radio.read(&rx_data, sizeof(uint16_t) + 3 * sizeof(float));
+    }
+//    radio.stopListening();
+//    radio.startListening();
+    Serial.write(rx_data, PacketSize);
+  }
+}
